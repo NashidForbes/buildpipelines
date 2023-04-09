798dcbed6b0e407814b0b6d77e4d846a562f0705
NashidForbes <50852214+NashidForbes@users.noreply.github.com>
2023-04-08 23:44:00 -0400

    Update SomeIntegrationTest.java

--- calculator-service/src/test/java/com/jetbrains/teamcity/calculatorservice/SomeIntegrationTest.java
+++ calculator-service/src/test/java/com/jetbrains/teamcity/calculatorservice/SomeIntegrationTest.java
@@ -57,6 +57,6 @@ public class SomeIntegrationTest {
         }
         Files.write(path.resolve("application.log"), hello.getBytes());
         Thread.sleep(1000);
-        assertNotNull("Hello variable should not be null.", hello.getBytes());
+        assertNotNull(hello);
     }
 }
