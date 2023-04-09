b05fa7263203a4e697250cd35accb726896f9b25
NashidForbes <50852214+NashidForbes@users.noreply.github.com>
2023-04-09 00:01:37 -0400

    Update SomeIntegrationTest.java

--- calculator-service/src/test/java/com/jetbrains/teamcity/calculatorservice/SomeIntegrationTest.java
+++ calculator-service/src/test/java/com/jetbrains/teamcity/calculatorservice/SomeIntegrationTest.java
@@ -53,7 +53,9 @@ public class SomeIntegrationTest {
         Files.write(subfolder.resolve("test.txt"), "test".getBytes());
 
         try (InputStream is = SomeIntegrationTest.class.getResourceAsStream("/sample-mp4-file.mp4")) {
-            Files.copy(is, path.resolve("selenium_chrome_recording.mp4"));
+            if(!files.exists(path.resolve("selenium_chrome_recording.mp4"))){
+              Files.copy(is, path.resolve("selenium_chrome_recording.mp4"));
+            }    
         }
         Files.write(path.resolve("application.log"), hello.getBytes());
         Thread.sleep(1000);
